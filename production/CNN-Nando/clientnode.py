b25728350fe7848a9fcc07f746b414a18b8a2647
Karol Majek <karolmajek@gmail.com>
2017-03-31 23:42:41 +0200

    nando fixed

--- production/CNN-Nando/clientnode.py
+++ production/CNN-Nando/clientnode.py
@@ -9,7 +9,6 @@ import h5py
 from keras import __version__ as keras_version
 import tensorflow as tf
 from keras import backend as K
-from Preprocess import *
 import cv2
 import time
 from data_buffer import DataBuffer
@@ -17,7 +16,7 @@ import queue
 import threading
 
 
-f = h5py.File("multiModel.h5", mode='r')
+f = h5py.File("model.h5", mode='r')
 model_version = f.attrs.get('keras_version')
 keras_version = str(keras_version).encode('utf8')
 
@@ -73,7 +72,7 @@ def make_prediction():
 				if jpeg_image:
 					image = np.array(Image.frombytes('RGB', [960,480], jpeg_image, 'raw'))
 					image_array = np.asarray(image)
-					image_array = cv2.cvtColor(image_array, cv2.COLOR_GBR2RGB)
+					image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
 					image_array = cv2.resize(image_array, (160, 80))
 					prediction = model.predict(image_array[None, :, :, :], batch_size=1)[0]
 					steering_angle = float(prediction[0])
@@ -87,7 +86,7 @@ def make_prediction():
 						throttle = 0
 
 
-					print(steering_angle, throttle, brake)
+					print('prediction: ',steering_angle, throttle, brake)
 
 					if res_queue.full(): # maintain a single most recent prediction in the queue
 						res_queue.get(False)
