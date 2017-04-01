11eee7d02272b5e193a12e24d41e3d28088162ea
Harinando Andrianarimanana <toni.andrianaht@gmail.com>
2017-04-01 01:25:45 -0700

    updated speed + cnn client node

--- production/CNN-Nando-SPEED/clientnode.py
+++ production/CNN-Nando-SPEED/clientnode.py
@@ -9,15 +9,16 @@ import h5py
 from keras import __version__ as keras_version
 import tensorflow as tf
 from keras import backend as K
-from Preprocess import *
+from PreprocessPsync import *
 import cv2
 import time
 from data_buffer import DataBuffer
 import queue
 import threading
+import ctypes
 
 
-f = h5py.File("psyncModel.h5", mode='r')
+f = h5py.File("psyncModelBase.h5", mode='r')
 model_version = f.attrs.get('keras_version')
 keras_version = str(keras_version).encode('utf8')
 
@@ -28,15 +29,13 @@ if model_version != keras_version:
 def customLoss(y_true, y_pred):
 	return K.mean(K.square(y_pred - y_true), axis=-1)
 
-model = load_model("psyncModel.h5", custom_objects={'customLoss':customLoss})
+model = load_model("psyncModelBase.h5", custom_objects={'customLoss':customLoss})
 graph = tf.get_default_graph()
 
 data_buffer = DataBuffer()
 res_queue = queue.Queue(maxsize=1)
 
-idxs = [0, 1, 2]
-means = [-2.135234308696, 0.690051203865, 62.68238949]
-stds = [0.000022089013, 0.000045442627, 13.48539298]
+MAX = 33.706074
 
 debug = False
 
@@ -49,28 +48,19 @@ def normalize_vector(xVec):
 
 
 def copyImage(byte_array, imageSize):
-	if imageSize > 8:
-		resize(byte_array, imageSize)
-		image = []
-		for i in range(imageSize):
-			image.append(byte_array[i])
-		return array.array('B', image).tostring()
-	return byte_array
+    new_array = np.ctypeslib.as_array(byte_array,shape=(imageSize,)).reshape((960, 480, 3))
+    return new_array
+	
 
 
 def imageReceived(imageSize, rawImage, speed, lat, lon):
-	print("image received with: ", speed, lat, lon)
 	jpegImage = copyImage(rawImage, imageSize)
 	data_buffer.add_item((jpegImage, speed, lat, lon))
 	
 Node = MainNode(imageReceived)
 
-MAX = 33.706074
-
-
 def make_prediction():
 	global graph
-	print('make prediction')
 	while True:
 		with graph.as_default():
 			item = data_buffer.get_item_for_processing()
@@ -79,27 +69,23 @@ def make_prediction():
 				speed = item[1]
 				lat = item[2]
 				lon = item[3]
-
-				if jpeg_image:
-					image = np.array(Image.frombytes('RGB', [960,480], jpeg_image, 'raw'))
-					image_array = np.asarray(image)
-					image_array = cv2.resize(image_array, (160, 80))
-					prediction = model.predict([ image_array[None, :, :, :], float(speed)/MAX ], batch_size=1)[0]
-					steering_angle = float(prediction[0])
-					throttle = float(prediction[1])
-					brake = float(prediction[2])
-
-					if brake > 0.2:
-						throttle = 0
-					else:
-						brake = 0
-
-					if speed > 23:
-						throttle = 0
-
-					if res_queue.full(): # maintain a single most recent prediction in the queue
-						res_queue.get(False)
-					res_queue.put((steering_angle, throttle, brake))
+				xVec = np.array([lon, lat, speed])
+				norm_xVec = normalize_vector(xVec)
+				image = np.array(Image.frombytes('RGB', [960,480], jpeg_image, 'raw'))
+				image_array = np.asarray(image)
+				image_array = cv2.resize(image_array, (160, 80))
+				prediction = model.predict([image_array[None, :, :, :],np.vstack([float(speed)/MAX])], batch_size=1)[0]
+				steering_angle = float(prediction[0])
+				throttle = float(prediction[1])
+				brake = float(prediction[2])
+				############################# Steering angles ##########################################################
+				if brake > 0.5:
+					throttle = 0.
+				else:
+					brake = 0.
+				if speed > 23.0:
+					throttle = 0.
+				res_queue.put((steering_angle[0][0], throttle, brake))
 
 
 def sendValues():
@@ -112,7 +98,6 @@ def sendValues():
 			steer = c_float(prediction[0])
 			throttle = c_float(prediction[1])
 			brake = c_float(prediction[2])
-			print("got values: ", steer, throttle, brake)
 		except queue.Empty:
 			pass
 		Node.steerCommand(steer)
@@ -126,6 +111,7 @@ thread.start()
 thread2 = threading.Thread(target=sendValues, args=())
 thread2.daemon = True
 thread2.start()
+
 
 
 Node.connectPolySync()
