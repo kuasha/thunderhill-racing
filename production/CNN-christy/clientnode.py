7a2410b066c357b17efc7c19868edb15eaaac934
Nadia Yudina <joystate@me.com>
2017-04-01 04:49:25 -0400

    fix christy client node

--- production/CNN-christy/clientnode.py
+++ production/CNN-christy/clientnode.py
@@ -19,28 +19,14 @@ import base64
 import json
 
 import numpy as np
-#import socketio
-#import eventlet
-#import eventlet.wsgi
 import time
 from PIL import Image
 from PIL import ImageOps
-#from flask import Flask, render_template
-#from io import BytesIO
 
 from keras.models import model_from_json
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
 
-# Fix error with Keras and TensorFlow
-#import tensorflow as tf
 import cv2
-#import matplotlib.pyplot as plt
-
-
-
-def customLoss(y_true, y_pred):
-    return K.mean(K.square(y_pred - y_true), axis=-1)
-
 
 
 f = h5py.File("thunderhill.h5", mode='r')
@@ -51,45 +37,19 @@ if model_version != keras_version:
 	print('You are using Keras version ', keras_version, ', but the model was built using ', model_version)
 
 model = load_model("thunderhill.h5")
-#
-# with open('model.json', 'r') as jfile:
-#     model = model_from_json(jfile.read())
-#
-# model.compile("adam", "mse")
-# weights_file = 'weights.0011-16.389.hdf5'
-# model.load_weights(weights_file)
 
-# model = load_model("multiModel.h5", custom_objects={'customLoss':customLoss})
 graph = tf.get_default_graph()
 
 data_buffer = DataBuffer()
 res_queue = queue.Queue(maxsize=1)
-#
-# idxs = [0, 1, 2]
-# means = [-122.33790211, 39.53881540, 62.68238949]
-# stds = [0.00099555, 0.00180817, 13.48539298]
-
-
-# def normalize_vector(xVec):
-#     for i, mean, std in zip(idxs, means, stds):
-#         xVec[i] -= mean
-#         xVec[i] /= std
-#     return xVec
 
 
 def copyImage(byte_array, imageSize):
-    if imageSize > 8:
-        resize(byte_array, imageSize)
-        image = []
-        for i in range(imageSize):
-            image.append(byte_array[i])
-        return array.array('B', image).tostring()
-    return byte_array
+    new_array = np.ctypeslib.as_array(byte_array,shape=(imageSize,)).reshape((960, 480, 3))
+    return new_array
 
 
 def imageReceived(imageSize, rawImage, speed, lat, lon):
-    # print("in image received")
-    # print(speed, lat, lon)
     jpegImage = copyImage(rawImage, imageSize)
     data_buffer.add_item((jpegImage, speed, lat, lon))
     try:
@@ -115,39 +75,36 @@ def make_prediction():
                 speed = item[1]
                 lat = item[2]
                 lon = item[3]
-                # xVec = np.array([lon, lat, speed])
-                # norm_xVec = normalize_vector(xVec)
-                # print(jpeg_image)
-                if jpeg_image:
-                    img = np.array(Image.frombytes('RGB', [960,480], jpeg_image, 'raw'))
-                    image_array = cv2.resize(img, (320, 160))[::-1,:,:]
-                    image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
+                
+                img = np.array(Image.frombytes('RGB', [960,480], jpeg_image, 'raw'))
+                image_array = cv2.resize(img, (320, 160))[::-1,:,:]
+                image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
 
-                    output = model.predict(image_array[None, :, :, :], batch_size=1)
-                    steering_angle = output[0][0]*-2
-                    throttle = output[0][1]
+                output = model.predict(image_array[None, :, :, :], batch_size=1)
+                steering_angle = output[0][0]*-2
+                throttle = output[0][1]
 
-                    min_speed = 8 * 0.44704
-                    max_speed = 50 * 0.44704
+                min_speed = 8 * 0.44704
+                max_speed = 50 * 0.44704
 
-                    if float(speed) > 25 * 0.44704:
-                        throttle = 0
+                if float(speed) > 25 * 0.44704:
+                    throttle = 0
 
-                    if steering_angle > 0.2 or steering_angle < -0.2:
-                        throttle = -throttle
+                if steering_angle > 0.2 or steering_angle < -0.2:
+                    throttle = -throttle
 
 
-                    brake=0
-                    if throttle<0:
-                        throttle=0
-                        brake=np.abs(throttle)
+                brake=0
+                if throttle<0:
+                    throttle=0
+                    brake=np.abs(throttle)
 
-                    print('prediction:',steering_angle,throttle,brake, 'speed: ',speed/0.44704)
+                print('prediction:',steering_angle,throttle,brake, 'speed: ',speed/0.44704)
 
-                    if res_queue.full(): # maintain a single most recent prediction in the queue
-                        res_queue.get(False)
+                if res_queue.full(): # maintain a single most recent prediction in the queue
+                    res_queue.get(False)
 
-                    res_queue.put((steering_angle, throttle, brake))
+                res_queue.put((steering_angle, throttle, brake))
                 end = time.time()
                 print('\n%.3f ms\n'%(end - start))
 
