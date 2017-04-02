fc3dcf867d191a5312628bb23013efcc6012f8e1
diyjac <diyjac@gmail.com>
2017-04-02 10:43:30 -0400

    feat: update joystick structure

--- production/CNN-Nando-SPEED-Joystick/MainNode/MainNode.py
+++ production/CNN-Nando-SPEED-Joystick/MainNode/MainNode.py
@@ -1,4 +1,6 @@
 from ctypes import *
+import pygame
+
 lib = cdll.LoadLibrary('./MainNode/libMainNode.so')
 
 class MainNode(object):
@@ -11,14 +13,37 @@ class MainNode(object):
         self.func1 = self.FUNC1(self.imagefunc)
         lib.MainNode_setImageCallback(self.obj, self.func1)
 
+        # initialize pygame and joystick
+        pygame.init()
+        pygame.joystick.init()
+
+
     def steerCommand(self, angle):
-    	lib.MainNode_steerCommand(self.obj, angle)
+        # We are using PS3 left joystick: so axis (0,1) run in pairs, left/right for 2, up/down for 3
+        # Change this if you want to switch to another axis on your joystick!
+        # Normally they are centered on (0,0)
+        leftright = joystick.get_axis(0)
+    	lib.MainNode_steerCommand(self.obj, angle+leftright)
 
     def brakeCommand(self, value):
-        lib.MainNode_brakeCommand(self.obj, value)
+        # We are using PS3 left joystick: so axis (0,1) run in pairs, left/right for 2, up/down for 3
+        # Change this if you want to switch to another axis on your joystick!
+        # Normally they are centered on (0,0)
+        updown = joystick.get_axis(0)
+        if updown < 0.0:
+            updown = -updown
+        else:
+            updown = 0.0
+        lib.MainNode_brakeCommand(self.obj, value+updown)
 
     def throttleCommand(self, value):
-        lib.MainNode_throttleCommand(self.obj, value)
+        # We are using PS3 left joystick: so axis (0,1) run in pairs, left/right for 2, up/down for 3
+        # Change this if you want to switch to another axis on your joystick!
+        # Normally they are centered on (0,0)
+        updown = joystick.get_axis(0)
+        if updown < 0.0:
+            updown = 0.0
+        lib.MainNode_throttleCommand(self.obj, value+updown)
 
     def connectPolySync(self):
         lib.MainNode_connectPolySync(self.obj)
