95f56844a5b83777160b6471c2894e15dfa70b8e
diyjac <diyjac@gmail.com>
2017-04-02 11:25:19 -0400

    feat: fix joystick initialization

--- production/CNN-Nando-SPEED-Joystick/MainNode/MainNode.py
+++ production/CNN-Nando-SPEED-Joystick/MainNode/MainNode.py
@@ -16,20 +16,23 @@ class MainNode(object):
 		# initialize pygame and joystick
 		pygame.init()
 		pygame.joystick.init()
+		self.joystick = pygame.joystick.Joystick(0)
+		self.joystick.init()
+
 
 
 	def steerCommand(self, angle):
 		# We are using PS3 left joystick: so axis (0,1) run in pairs, left/right for 2, up/down for 3
 		# Change this if you want to switch to another axis on your joystick!
 		# Normally they are centered on (0,0)
-		leftright = joystick.get_axis(0)
+		leftright = self.joystick.get_axis(0)
 		lib.MainNode_steerCommand(self.obj, angle+leftright)
 
 	def brakeCommand(self, value):
 		# We are using PS3 left joystick: so axis (0,1) run in pairs, left/right for 2, up/down for 3
 		# Change this if you want to switch to another axis on your joystick!
 		# Normally they are centered on (0,0)
-		updown = joystick.get_axis(1)
+		updown = self.joystick.get_axis(1)
 		if updown < 0.0:
 			updown = -updown
 		else:
@@ -40,7 +43,7 @@ class MainNode(object):
 		# We are using PS3 left joystick: so axis (0,1) run in pairs, left/right for 2, up/down for 3
 		# Change this if you want to switch to another axis on your joystick!
 		# Normally they are centered on (0,0)
-		updown = joystick.get_axis(1)
+		updown = self.joystick.get_axis(1)
 		if updown < 0.0:
 			updown = 0.0
 		lib.MainNode_throttleCommand(self.obj, value+updown)
